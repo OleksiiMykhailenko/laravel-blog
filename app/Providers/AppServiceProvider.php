fb9406fce486e0920edd1c09e3deef03389a2242
Oleksii Mykhailenko <mykhailenko.oleksii33@gmail.com>
2020-05-25 19:55:36 +0300

    first commit

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Providers;
+
+use App\Category;
+use App\Post;
+use App\Comment;
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        Schema::defaultStringLength(191);
+        view()->composer('pages._sidebar', function($view) {
+            $view->with('popularPosts', Post::getPopularPosts());
+            $view->with('featuredPosts', Post::getFeaturedPosts());
+            $view->with('recentPosts', Post::getRecentPosts());
+            $view->with('categories', Category::all());
+        });
+
+        view()->composer('admin._sidebar', function($view) {
+            $view->with('newCommentsCount', Comment::where('status', 0)->count());
+        });
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
---
+++ resources/views/emails/verify.blade.php
@@ -0,0 +1,2 @@
+<h1>Thanks, click on the link!</h1>
+<a href="http://blog/verify/{{$subs->token}}">{{$subs->token}}</a>
